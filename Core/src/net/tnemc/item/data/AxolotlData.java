aaec881ed7761390dee59fd8839efe929ee3a7c8
creatorfromhell <daniel.viddy@gmail.com>
2022-07-15 19:56:22 -0400

    Add Axolotl bucket support.

---
+++ Core/src/net/tnemc/item/data/AxolotlData.java
@@ -0,0 +1,35 @@
+package net.tnemc.item.data;
+
+import net.tnemc.item.JSONHelper;
+import net.tnemc.item.SerialItemData;
+import org.json.simple.JSONObject;
+
+public abstract class AxolotlData<T> implements SerialItemData<T> {
+
+  private String variant;
+
+  /**
+   * Converts the {@link SerialItemData} to a JSON object.
+   *
+   * @return The JSONObject representing this {@link SerialItemData}.
+   */
+  @Override
+  public JSONObject toJSON() {
+    JSONObject json = new JSONObject();
+    json.put("name", "axolotl");
+    json.put("variant", variant);
+    return json;
+  }
+
+  /**
+   * Reads JSON data and converts it back to a {@link SerialItemData} object.
+   *
+   * @param json The JSONHelper instance of the json data.
+   */
+  @Override
+  public void readJSON(JSONHelper json) {
+    if(json.has("variant")) {
+      this.variant = json.getString("variant");
+    }
+  }
+}
