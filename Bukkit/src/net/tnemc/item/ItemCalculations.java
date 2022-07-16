7f6396ea9b39eabd2110bfd527c4863cea104a35
creatorfromhell <daniel.viddy@gmail.com>
2022-07-16 09:47:45 -0400

    The New Item Library is LGPL.

--- Bukkit/src/net/tnemc/item/ItemCalculations.java
+++ Bukkit/src/net/tnemc/item/ItemCalculations.java
@@ -1,5 +1,24 @@
 package net.tnemc.item;
 
+/*
+ * The New Economy Minecraft Server Plugin
+ *
+ * Copyright (C) 2022 Daniel "creatorfromhell" Vidmar
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 import net.tnemc.item.providers.CalculationsProvider;
 import org.bukkit.Bukkit;
@@ -17,17 +36,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
-
-/*
- * The New Economy Minecraft Server Plugin
- * <p>
- * Created by Daniel on 6/3/2018.
- * <p>
- * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ or send a letter to
- * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
- * Created by creatorfromhell on 06/30/2017.
- */
 
 /**
  * Represents a Bukkit implementation of the {@link CalculationsProvider}.
--- Core/src/net/tnemc/item/data/banner/PatternData.java
+++ Core/src/net/tnemc/item/data/banner/PatternData.java
@@ -1,6 +1,24 @@
 package net.tnemc.item.data.banner;
 
-import java.util.HashMap;
+/*
+ * The New Economy Minecraft Server Plugin
+ *
+ * Copyright (C) 2022 Daniel "creatorfromhell" Vidmar
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 public class PatternData {
 
@@ -26,57 +44,5 @@ public class PatternData {
 
   public void setPattern(String pattern) {
     this.pattern = pattern;
-  }
-
-  /**
-   * Indicates whether some other object is "equal to" this one.
-   * <p>
-   * The {@code equals} method implements an equivalence relation
-   * on non-null object references:
-   * <ul>
-   * <li>It is <i>reflexive</i>: for any non-null reference value
-   *     {@code x}, {@code x.equals(x)} should return
-   *     {@code true}.
-   * <li>It is <i>symmetric</i>: for any non-null reference values
-   *     {@code x} and {@code y}, {@code x.equals(y)}
-   *     should return {@code true} if and only if
-   *     {@code y.equals(x)} returns {@code true}.
-   * <li>It is <i>transitive</i>: for any non-null reference values
-   *     {@code x}, {@code y}, and {@code z}, if
-   *     {@code x.equals(y)} returns {@code true} and
-   *     {@code y.equals(z)} returns {@code true}, then
-   *     {@code x.equals(z)} should return {@code true}.
-   * <li>It is <i>consistent</i>: for any non-null reference values
-   *     {@code x} and {@code y}, multiple invocations of
-   *     {@code x.equals(y)} consistently return {@code true}
-   *     or consistently return {@code false}, provided no
-   *     information used in {@code equals} comparisons on the
-   *     objects is modified.
-   * <li>For any non-null reference value {@code x},
-   *     {@code x.equals(null)} should return {@code false}.
-   * </ul>
-   * <p>
-   * The {@code equals} method for class {@code Object} implements
-   * the most discriminating possible equivalence relation on objects;
-   * that is, for any non-null reference values {@code x} and
-   * {@code y}, this method returns {@code true} if and only
-   * if {@code x} and {@code y} refer to the same object
-   * ({@code x == y} has the value {@code true}).
-   * <p>
-   * Note that it is generally necessary to override the {@code hashCode}
-   * method whenever this method is overridden, so as to maintain the
-   * general contract for the {@code hashCode} method, which states
-   * that equal objects must have equal hash codes.
-   *
-   * @param obj the reference object with which to compare.
-   *
-   * @return {@code true} if this object is the same as the obj
-   * argument; {@code false} otherwise.
-   * @see #hashCode()
-   * @see HashMap
-   */
-  @Override
-  public boolean equals(Object obj) {
-    return super.equals(obj);
   }
 }
