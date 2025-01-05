3a0f3473c82888a0cee071f58dd53ba143ed3f6b
Daniel V. <daniel.viddy@gmail.com>
2025-01-05 17:08:27 -0500

    Add TrimComponent

---
+++ Core/src/net/tnemc/item/component/impl/TrimComponent.java
@@ -0,0 +1,77 @@
+package net.tnemc.item.component.impl;
+/*
+ * The New Item Library
+ * Copyright (C) 2022 - 2025 Daniel "creatorfromhell" Vidmar
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import net.tnemc.item.AbstractItemStack;
+import net.tnemc.item.JSONHelper;
+import net.tnemc.item.SerialItemData;
+import net.tnemc.item.component.SerialComponent;
+import net.tnemc.item.component.TooltippableSerialComponent;
+import net.tnemc.item.platform.ItemPlatform;
+import org.json.simple.JSONObject;
+
+import java.util.Objects;
+
+/**
+ * TrimComponent
+ *
+ * @author creatorfromhell
+ * @see <a href="https://minecraft.wiki/w/Data_component_format#trim">Reference</a>
+ * <p>
+ * @since 0.2.0.0
+ */
+public abstract class TrimComponent<T> extends TooltippableSerialComponent<T> {
+
+  protected String pattern;
+  protected String material;
+
+  @Override
+  public String getType() {
+    return "trim";
+  }
+
+  @Override
+  public JSONObject toJSON() {
+    final JSONObject json = new JSONObject();
+    json.put("pattern", pattern);
+    json.put("material", material);
+    json.put("show_in_tooltip", showInTooltip);
+    return json;
+  }
+
+  @Override
+  public <I extends AbstractItemStack<T>> void readJSON(final JSONHelper json, final ItemPlatform<I, T> platform) {
+    super.readJSON(json, platform); // Read tooltip visibility
+    pattern = json.getString("pattern");
+    material = json.getString("material");
+  }
+
+  @Override
+  public boolean equals(final SerialComponent<? extends T> component) {
+    if (!(component instanceof final TrimComponent<?> other)) return false;
+    return showInTooltip == other.showInTooltip &&
+           Objects.equals(this.pattern, other.pattern) &&
+           Objects.equals(this.material, other.material);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), pattern, material);
+  }
+}
