942fa63d998133045a585b31d17cf5d521d82509
Daniel V. <daniel.viddy@gmail.com>
2025-01-05 17:08:58 -0500

    Add WritableBookContentComponent

---
+++ Core/src/net/tnemc/item/component/impl/WritableBookContentComponent.java
@@ -0,0 +1,75 @@
+package net.tnemc.item.component.impl;
+/*
+ * The New Item Library
+ * Copyright (C) 2022 - 2025 Daniel "creatorfromhell" Vidmar
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import net.tnemc.item.AbstractItemStack;
+import net.tnemc.item.JSONHelper;
+import net.tnemc.item.SerialItemData;
+import net.tnemc.item.component.SerialComponent;
+import net.tnemc.item.platform.ItemPlatform;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * WrittableBookContentComponent
+ *
+ * @author creatorfromhell
+ * @see <a href="https://minecraft.wiki/w/Data_component_format#writable_book_content">Reference</a>
+ * <p>
+ * @since 0.2.0.0
+ */
+public abstract class WritableBookContentComponent<T> implements SerialComponent<T> {
+
+  protected final List<String> pages = new ArrayList<>();
+
+  @Override
+  public String getType() {
+    return "writable_book_content";
+  }
+
+  @Override
+  public JSONObject toJSON() {
+    final JSONObject json = new JSONObject();
+    final JSONArray pagesArray = new JSONArray();
+    pagesArray.addAll(pages);
+    json.put("pages", pagesArray);
+    return json;
+  }
+
+  @Override
+  public <I extends AbstractItemStack<T>> void readJSON(final JSONHelper json, final ItemPlatform<I, T> platform) {
+    pages.clear();
+    pages.addAll(json.getStringList("pages"));
+  }
+
+  @Override
+  public boolean equals(final SerialComponent<? extends T> component) {
+    if (!(component instanceof final WritableBookContentComponent<?> other)) return false;
+    return Objects.equals(this.pages, other.pages);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(pages);
+  }
+}
