0fc60048deb1b69c48d9fb9f90feb2238e3358eb
Daniel V. <daniel.viddy@gmail.com>
2025-01-05 17:07:15 -0500

    Add RecipesComponent

---
+++ Core/src/net/tnemc/item/component/impl/RecipesComponent.java
@@ -0,0 +1,75 @@
+package net.tnemc.item.component.impl;
+/*
+ * The New Item Library
+ * Copyright (C) 2022 - 2025 Daniel "creatorfromhell" Vidmar
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import net.tnemc.item.AbstractItemStack;
+import net.tnemc.item.JSONHelper;
+import net.tnemc.item.SerialItemData;
+import net.tnemc.item.component.SerialComponent;
+import net.tnemc.item.platform.ItemPlatform;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * RecipesComponent
+ *
+ * @author creatorfromhell
+ * @see <a href="https://minecraft.wiki/w/Data_component_format#recipes">Reference</a>
+ * <p>
+ * @since 0.2.0.0
+ */
+public abstract class RecipesComponent<T> implements SerialComponent<T> {
+
+  protected final List<String> recipes = new ArrayList<>();
+
+  @Override
+  public String getType() {
+    return "recipes";
+  }
+
+  @Override
+  public JSONObject toJSON() {
+    final JSONObject json = new JSONObject();
+    final JSONArray recipesArray = new JSONArray();
+    recipesArray.addAll(recipes);
+    json.put("recipes", recipesArray);
+    return json;
+  }
+
+  @Override
+  public <I extends AbstractItemStack<T>> void readJSON(final JSONHelper json, final ItemPlatform<I, T> platform) {
+    recipes.clear();
+    recipes.addAll(json.getStringList("recipes"));
+  }
+
+  @Override
+  public boolean equals(final SerialComponent<? extends T> component) {
+    if (!(component instanceof final RecipesComponent<?> other)) return false;
+    return Objects.equals(this.recipes, other.recipes);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(recipes);
+  }
+}
