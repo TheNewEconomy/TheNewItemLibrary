647d36c16277fcb9af351694cc0835f45a016651
Daniel V. <daniel.viddy@gmail.com>
2025-01-05 01:38:02 -0500

    BaseColorComponent

---
+++ Core/src/net/tnemc/item/component/impl/BaseColorComponent.java
@@ -0,0 +1,108 @@
+package net.tnemc.item.component.impl;
+/*
+ * The New Item Library
+ * Copyright (C) 2022 - 2025 Daniel "creatorfromhell" Vidmar
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import net.tnemc.item.AbstractItemStack;
+import net.tnemc.item.JSONHelper;
+import net.tnemc.item.SerialItemData;
+import net.tnemc.item.component.SerialComponent;
+import net.tnemc.item.platform.ItemPlatform;
+import org.json.simple.JSONObject;
+
+import java.util.Objects;
+
+/**
+ * BaseColorComponent
+ *
+ * @author creatorfromhell
+ * @since 0.2.0.0
+ */
+
+public abstract class BaseColorComponent<T> implements SerialComponent<T> {
+
+  protected String color;
+
+  /**
+   * @return the type of component this is.
+   */
+  @Override
+  public String getType() {
+    return "base_color";
+  }
+
+  /**
+   * Converts this component's data to a JSON object.
+   *
+   * @return The JSONObject representing this component's data.
+   */
+  @Override
+  public JSONObject toJSON() {
+    final JSONObject json = new JSONObject();
+    json.put("color", color);
+    return json;
+  }
+
+  /**
+   * Reads JSON data and converts it back to this component's data.
+   *
+   * @param json The JSONHelper instance of the JSON data.
+   * @param platform The ItemPlatform instance.
+   */
+  @Override
+  public <I extends AbstractItemStack<T>> void readJSON(final JSONHelper json, final ItemPlatform<I, T> platform) {
+    if(json.has("color")) {
+      color = json.getString("color");
+    }
+  }
+
+  /**
+   * Used to determine if some data is equal to this data. This means that it has to be an exact
+   * copy of this data.
+   *
+   * @param component The component to compare.
+   * @return True if similar, otherwise false.
+   */
+  @Override
+  public boolean equals(final SerialComponent<? extends T> component) {
+    if(!(component instanceof final BaseColorComponent<?> other)) return false;
+
+    return Objects.equals(this.color, other.color);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(color);
+  }
+
+  /**
+   * @return The current base color.
+   */
+  public String getColor() {
+    return color;
+  }
+
+  /**
+   * Sets the base color for this component.
+   *
+   * @param color The color to set.
+   */
+  public void setColor(final String color) {
+    this.color = color;
+  }
+}
