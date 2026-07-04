f288bd52638a4343271b23e7f087eadfbdf4dcd3
Daniel V. <daniel.viddy@gmail.com>
2026-07-04 01:19:55 -0400

    Added PaperWeaponComponent

---
+++ Paper/src/main/java/net/tnemc/item/paper/platform/impl/modern/PaperWeaponComponent.java
@@ -0,0 +1,93 @@
+package net.tnemc.item.paper.platform.impl.modern;
+/*
+ * The New Item Library
+ * Copyright (C) 2022 - 2026 Daniel "creatorfromhell" Vidmar
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.Weapon;
+import net.tnemc.item.component.impl.WeaponComponent;
+import net.tnemc.item.paper.PaperItemStack;
+import net.tnemc.item.providers.VersionUtil;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Optional;
+
+/**
+ * PaperWeaponComponent
+ *
+ * @author creatorfromhell
+ * @since 0.2.0.0
+ */
+public class PaperWeaponComponent extends WeaponComponent<PaperItemStack, ItemStack> {
+
+  public PaperWeaponComponent() {
+
+  }
+
+  public PaperWeaponComponent(final int damagePerAttack, final boolean canDisableBlocking) {
+
+    super(damagePerAttack, canDisableBlocking);
+  }
+
+  @Override
+  public boolean enabled(final String version) {
+
+    return VersionUtil.isOneTwentyOneEleven(version);
+  }
+
+  @Override
+  public boolean appliesTo(final ItemStack item) {
+
+    return true;
+  }
+
+  @Override
+  public ItemStack apply(final PaperItemStack serialized, final ItemStack item) {
+
+    final Optional<PaperWeaponComponent> componentOptional = serialized.component(identifier());
+    if(componentOptional.isEmpty()) {
+      return item;
+    }
+
+    final PaperWeaponComponent component = componentOptional.get();
+
+    item.setData(DataComponentTypes.WEAPON, Weapon.weapon()
+            .itemDamagePerAttack(component.damagePerAttack())
+            .disableBlockingForSeconds(component.canDisableBlocking()? 5.0f : 0.0f));
+
+    return item;
+  }
+
+  @Override
+  public PaperItemStack serialize(final ItemStack item, final PaperItemStack serialized) {
+
+    final Weapon weapon = item.getData(DataComponentTypes.WEAPON);
+    if(weapon == null) {
+      return serialized;
+    }
+
+    final PaperWeaponComponent component = (serialized.paperComponent(identifier()) instanceof final WeaponComponent<?, ?> getComponent)?
+                                           (PaperWeaponComponent)getComponent : new PaperWeaponComponent();
+
+    component.damagePerAttack(weapon.itemDamagePerAttack());
+    component.canDisableBlocking(weapon.disableBlockingForSeconds() > 0.0f);
+
+    serialized.applyComponent(component);
+    return serialized;
+  }
+}
