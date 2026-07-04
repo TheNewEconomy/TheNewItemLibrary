a69989085efbc30e674ffd04c7fd989944f6fd46
Daniel V. <daniel.viddy@gmail.com>
2026-07-03 21:40:00 -0400

    Added PaperTooltipStyleComponent

---
+++ Paper/src/main/java/net/tnemc/item/paper/platform/impl/modern/PaperTooltipStyleComponent.java
@@ -0,0 +1,87 @@
+package net.tnemc.item.paper.platform.impl.modern;
+/*
+ * The New Item Library
+ * Copyright (C) 2022 - 2026 Daniel "creatorfromhell" Vidmar
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import net.kyori.adventure.key.Key;
+import net.tnemc.item.component.impl.TooltipStyleComponent;
+import net.tnemc.item.paper.PaperItemStack;
+import net.tnemc.item.providers.VersionUtil;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Optional;
+
+/**
+ * PaperTooltipStyleComponent
+ *
+ * @author creatorfromhell
+ * @since 0.2.0.0
+ */
+public class PaperTooltipStyleComponent extends TooltipStyleComponent<PaperItemStack, ItemStack> {
+
+  public PaperTooltipStyleComponent() {
+
+  }
+
+  public PaperTooltipStyleComponent(final String style) {
+
+    super(style);
+  }
+
+  @Override
+  public boolean enabled(final String version) {
+
+    return VersionUtil.isOneTwentyOneFour(version);
+  }
+
+  @Override
+  public boolean appliesTo(final ItemStack item) {
+
+    return true;
+  }
+
+  @Override
+  public ItemStack apply(final PaperItemStack serialized, final ItemStack item) {
+
+    final Optional<PaperTooltipStyleComponent> componentOptional = serialized.component(identifier());
+    if(componentOptional.isEmpty()) {
+      return item;
+    }
+
+    item.setData(DataComponentTypes.TOOLTIP_STYLE, Key.key(componentOptional.get().style));
+    return item;
+  }
+
+  @Override
+  public PaperItemStack serialize(final ItemStack item, final PaperItemStack serialized) {
+
+    final Key style = item.getData(DataComponentTypes.TOOLTIP_STYLE);
+    if(style == null) {
+      return serialized;
+    }
+
+    final PaperTooltipStyleComponent component = (serialized.paperComponent(identifier()) instanceof final TooltipStyleComponent<?, ?> getComponent)?
+                                                 (PaperTooltipStyleComponent)getComponent : new PaperTooltipStyleComponent();
+
+    component.style(style.asString());
+
+    serialized.applyComponent(component);
+    return serialized;
+  }
+}
